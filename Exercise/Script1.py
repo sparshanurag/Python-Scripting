0073a1eb5e4e59052b91f6352d0868526075ee61
Sparsh Anurag <sparsh.anurag@tothenew.com>
2021-05-17 13:30:14 +0530

    Script1 added

---
+++ Exercise/Script1.py
@@ -0,0 +1,26 @@
+"""
+Write a Python script that sets the following variables:
+
+first_name - Set to your first name
+last_name - Set to your last name
+age - Set to your age as an integer
+birth_date - Set to your birthdate as a string
+Using the variables, print the following to the screen when you run the script:
+
+My name is FIRST_NAME LAST_NAME.
+I was born on BIRTH_DATE, and I'm AGE years old.
+
+"""
+
+#! usr/bin/env python3
+first_name = "Sparsh"
+last_name = "Anurag"
+age = 22
+birthdate = "01/01/1999"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birthdate} and I am {age} years old.")
+
+"""
+To run : $ python Script1.py
+"""
